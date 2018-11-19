f01cd1fb145350a7874b1625970017d868c115ab
hgldwskvn <38421249+hgldwskvn@users.noreply.github.com>
2018-11-19 17:46:46 +0900

    Update default.js

--- is/default.js
+++ is/default.js
@@ -1,3 +1,59 @@
+function setCookie(c_name,value,expriredays){
+  var extime=new Date().getTime();
+  var cltime=new Date(extime+(60*60*24*1000*expriredays));
+  var exdate=cltime.toUTCString();
+  var s="";
+  s+=c_name +"="escape(value);
+  s+="; path="+location.pathname;
+  if(expiredays){
+    s+=";expires="+exdate";";
+  }else{
+    s+=";";
+  }
+  document.cookie=s;
+}
+
+function getCookie(c_name){
+  var st="";
+  var ed="";
+  if(0<document.cookie.length){
+    st=document.cookie.indexOf(c_name+"=");
+    if(st!=-1){
+      st=st+c_name.length+1;
+      ed=document.cookieindexOF(";",st);
+      if(ed==-1) ed=document.cookie.length;
+      return unescape(document.cookie.substring(st,ed));
+    }
+  }
+  return"";
+}
+var last_date=getCookie('lastDate');
+if(last_date){
+  document.getElementById('cookie').textContent='前回訪れた時間:'+ last_date;
+}else{
+  document.getElementById('cookie').textContent='初めまして';
+  }
+
+var current_time=new Date();
+setCookie('lastDate',current_time.toString(),7);
+
+
+function getFileName(){
+  return window.location.href.split('/').pop();
+}
+var filename=getFileName();
+var opt;
+if(filename==='other.html'){
+ opt=document.querySelector('option[value="other.html"]');
+}else{
+  opt=document.querySelector('option[value="index.html"]');
+}
+opt.selected=true;
+
+document.getElementById('from').select.onchange=function(){
+  location.href=document.getElementById('from').select.value;
+}
+
 var separate_time=function(time){
   var sec =Math.floor((time/1000)%60);
   var min =Math.floor((time/1000/60)%60);
@@ -7,7 +63,8 @@ var separate_time=function(time){
   var year =time.getFullYear();
   return[sec,min,hours,days,month,year];
 }
-
+var filename=getFileName();
+var 
 var update=function(){
 var target=new Date();
 var target=new Date(2020,7.24.0.0.0.0);
